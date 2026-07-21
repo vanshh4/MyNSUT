6d09932ddc024b04152eb6f8ae2c128b92d1d687
Vansh Bhardwaj <vansh10958@gmail.com>
2026-07-21 11:19:41 +0530

    Add root package files

--- shared/dist/types/index.js
+++
@@ -1,3 +0,0 @@
-export { PERMISSIONS, PERMISSION_CODES, isPermissionCode } from "../constants/permissions.js";
-export { ROLES, ROLE_CODES, isRoleCode } from "../constants/roles.js";
-//# sourceMappingURL=index.js.map
